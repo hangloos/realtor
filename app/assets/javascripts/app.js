43f5c97d72247db98b4c62900132a7a4e763c1b1
Eric Loos <ericloos00@gmail.com>
2017-06-07 18:29:00 -0500

    test heroku connection to github

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,54 @@
+// (function () {
+//     'use strict'
+
+//     var app = angular.module('codyorr', ['ui.router']);
+
+// app.config(function($stateProvider, $urlRouterProvider){
+//   $urlRouterProvider.otherwise('/');
+
+//   $stateProvider
+//   .state({
+//     name: 'home',
+//     url: '/',
+//     templateUrl: 'pages/home.html'
+//   })
+
+//   .state({
+//     name: 'about',
+//     url: '/about',
+//     templateUrl: 'pages/about.html'
+//   })
+
+//   .state({
+//     name: 'blog',
+//     url: '/blog',
+//     templateUrl: 'pages/blog.html'
+//   })
+
+//   .state({
+//     name: 'contact',
+//     url: '/contact',
+//     templateUrl: 'pages/contact.html'
+//   })
+
+//   .state({
+//     name: 'listings',
+//     url: '/listings',
+//     templateUrl: 'pages/listings.html'
+//   })
+
+//   .state({
+//     name: 'mortgage',
+//     url: '/mortgage',
+//     templateUrl: 'pages/mortgage.html'
+//   })
+
+//   .state({
+//     name: 'valuation',
+//     url: '/valuation',
+//     templateUrl: 'pages/valuation.html'
+//   })
+   
+// });
+
+//   }())
