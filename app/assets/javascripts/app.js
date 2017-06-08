b2f2fda6829d40c6cc90b94b890377f47384a24e
Eric Loos <ericloos00@gmail.com>
2017-06-08 11:03:03 -0500

    added routes, module and layout for angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,54 +1,54 @@
-// (function () {
-//     'use strict'
+(function () {
+    'use strict'
 
-//     var app = angular.module('codyorr', ['ui.router']);
+    var app = angular.module('codyorr', ['ui.router', 'templates']);
 
-// app.config(function($stateProvider, $urlRouterProvider){
-//   $urlRouterProvider.otherwise('/');
+    app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
 
-//   $stateProvider
-//   .state({
-//     name: 'home',
-//     url: '/',
-//     templateUrl: 'pages/home.html'
-//   })
+  $stateProvider
+  .state({
+    name: 'home',
+    url: '/',
+    templateUrl: 'pages/home.html'
+  })
 
-//   .state({
-//     name: 'about',
-//     url: '/about',
-//     templateUrl: 'pages/about.html'
-//   })
+  .state({
+    name: 'about',
+    url: '/about',
+    templateUrl: 'pages/about.html'
+  })
 
-//   .state({
-//     name: 'blog',
-//     url: '/blog',
-//     templateUrl: 'pages/blog.html'
-//   })
+  .state({
+    name: 'blog',
+    url: '/blog',
+    templateUrl: 'pages/blog.html'
+  })
 
-//   .state({
-//     name: 'contact',
-//     url: '/contact',
-//     templateUrl: 'pages/contact.html'
-//   })
+  .state({
+    name: 'contact',
+    url: '/contact',
+    templateUrl: 'pages/contact.html'
+  })
 
-//   .state({
-//     name: 'listings',
-//     url: '/listings',
-//     templateUrl: 'pages/listings.html'
-//   })
+  .state({
+    name: 'listings',
+    url: '/listings',
+    templateUrl: 'pages/listings.html'
+  })
 
-//   .state({
-//     name: 'mortgage',
-//     url: '/mortgage',
-//     templateUrl: 'pages/mortgage.html'
-//   })
+  .state({
+    name: 'mortgage',
+    url: '/mortgage',
+    templateUrl: 'pages/mortgage.html'
+  })
 
-//   .state({
-//     name: 'valuation',
-//     url: '/valuation',
-//     templateUrl: 'pages/valuation.html'
-//   })
+  .state({
+    name: 'valuation',
+    url: '/valuation',
+    templateUrl: 'pages/valuation.html'
+  })
    
-// });
+});
 
-//   }())
+  }())
